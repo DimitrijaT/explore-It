f67430f8d24f7a6b71feff6db50ae6559edbd091
janevskistefan <38108655+janevskistefan@users.noreply.github.com>
2023-02-13 20:03:48 +0100

    merging front_end from demonstration branch to main

--- Domashna 3 i 4/Explore_It/explore_it_front_end/src/App.js
+++ Domashna 3 i 4/Explore_It/explore_it_front_end/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React from 'react';
 import { MapPage } from './pages/MapPage';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
@@ -6,31 +6,17 @@ import { NoPage } from './pages/NoPage';
 import { Navigation } from './pages/Navigation';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer } from './pages/Footer';
-import { Cookies } from 'react-cookie';
 
 export function App() {
-    const inputRef = useRef();
-    const [lang, setLang] = useState('mk');
-    const cookies = new Cookies();
-
-    useEffect(() => {
-        setLang(cookies.get('lang'));
-    }, []);
-
-    function translateTo(desiredLang) {
-        setLang(desiredLang);
-        cookies.set('lang', desiredLang, { maxAge: 3600 });
-    }
-
     return (
         <BrowserRouter>
-            <Navigation ref={inputRef} />
+            <Navigation />
             <Routes>
                 <Route path="/">
-                    <Route index element={<MapPage props={lang} />} />
-                    <Route path="about" element={<About props={lang} />} />
-                    <Route path="contact" element={<Contact props={lang} />} />
-                    <Route path="*" element={<NoPage props={lang} />} />
+                    <Route index element={<MapPage />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<NoPage/>} />
                 </Route>
             </Routes>
             <Footer />
--- Domashna 3 i 4/Explore_It/explore_it_front_end/src/pages/NoPage.js
+++ Domashna 3 i 4/Explore_It/explore_it_front_end/src/pages/NoPage.js
@@ -1,15 +1,41 @@
 import '../css/NoPage.css'
 import React from "react";
-import {t} from "i18next";
+import i18next, {t} from "i18next";
+import {Cookies} from "react-cookie";
 
 export class NoPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            language: "mk"
+        }
+    }
+
+
+    componentDidMount() {
+        let tempCookie = new Cookies().get("lang");
+        if (tempCookie === undefined) {
+            new Cookies().set("lang", "mk");
+            tempCookie = "mk";
+        }
+
+        if (this.state.language !== tempCookie) {
+            i18next.changeLanguage(tempCookie).then(() => {
+                this.setState({language: tempCookie})
+                new Cookies().set("lang", tempCookie, {maxAge: 3600});
+            });
+        }
+
+        setTimeout(() => {
+            document.location.href = '/';
+        }, 3000)
+    }
+
     render() {
         return (
             <div id="no_page_main">
-                {
-                    //TODO: 404 Page
-                    t("")
-                }
+                    <h1>{t("page_404")}</h1>
+                    <h4>{t("redirecting_404")}</h4>
             </div>
         );
     }
